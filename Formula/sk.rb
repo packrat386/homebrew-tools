1c0e144748fa50e0e72fe9e6efcd8dcd30f2d9e5
Aidan Coyle <packrat386@gmail.com>
2024-07-10 16:32:40 -0500

    Update formula for sk to v0.1.1

--- Formula/sk.rb
+++ Formula/sk.rb
@@ -1,8 +1,8 @@
 class Sk < Formula
-  desc "a tool for collecting and running scripts"
+  desc "Tool for collecting and running scripts"
   homepage "https://github.com/packrat386/sk"
-  url "https://github.com/packrat386/sk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8dd8e1b2e998b24a5b618c8138d381daf027659ea2ef8ca4bbfcb4620f71e4dc"
+  url "https://github.com/packrat386/sk/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "3d923f3303d41ce6d438dac0af06947f0d5ad19857a9e29456a17218d265b621"
   license "MIT"
 
   def install
@@ -12,16 +12,16 @@ class Sk < Formula
   def caveats
     <<~EOF
       Add the following to the ~/.bash_profile or ~/.zshrc:
-        [ -s #{pkgshare}/sk.sh ] && \. #{pkgshare}/sk.sh
+        [ -s #{pkgshare}/sk.sh ] && . #{pkgshare}/sk.sh
     EOF
   end
 
   test do
     (testpath/"script.sh").write <<~EOF
-[ -s #{pkgshare}/sk.sh ] && \. #{pkgshare}/sk.sh
+      [ -s #{pkgshare}/sk.sh ] && . #{pkgshare}/sk.sh
 
-sk help
-EOF
+      sk help
+    EOF
 
     system "bash", testpath/"script.sh"
   end
