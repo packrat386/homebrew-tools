0c4bbd11f46f735c8b8d88b3bfb4f5d79c186d39
Aidan Coyle <packrat386@gmail.com>
2025-10-23 00:07:17 -0500

    kamino v0.2.0

--- Formula/kamino.rb
+++ Formula/kamino.rb
@@ -1,8 +1,8 @@
 class Kamino < Formula
   desc "Cloning experts"
   homepage "https://github.com/packrat386/kamino"
-  url "https://github.com/packrat386/kamino/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e762856f9c001862a3ec2e510e0cfe49b4fe000f236f53b64701f186097ef883"
+  url "https://github.com/packrat386/kamino/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "30f1f8b8c0fca8ddcacd74403f9ccdf1a5b3965a5601647b09f1f2bd1b12b7d3"
   license "MIT"
 
   def install
