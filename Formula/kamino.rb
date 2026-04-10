5dd801e96c21ebe9a8a83fa129d9cccba8a909ee
Aidan Coyle <packrat386@gmail.com>
2026-04-10 00:10:30 -0500

    Fix kamino sha256

--- Formula/kamino.rb
+++ Formula/kamino.rb
@@ -2,7 +2,7 @@ class Kamino < Formula
   desc "Cloning experts"
   homepage "https://github.com/packrat386/kamino"
   url "https://github.com/packrat386/kamino/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "7d22ebd98b8b309112defe39e9ddd8abd25e02e94f8d0015e7113e14df17c362"
   license "MIT"
 
   def install
